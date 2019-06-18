12de99ddb1a23acecee22dc69ccfd87da2c8d79a
koseirem1 <koseirem1@gmail.com>
2019-06-18 15:03:54 +0300

    CompanyService ekle

--- Veresiye.UI/Program.cs
+++ Veresiye.UI/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Veresiye.Data;
+using Veresiye.Service;
+using static Veresiye.Service.ActivityService;
 
 namespace Veresiye.UI
 {
@@ -14,9 +18,27 @@ namespace Veresiye.UI
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMain());
+            var builder = new ContainerBuilder();
+            //autofac bağlantısı
+            builder.RegisterType<ApplicationDbContext>().As<ApplicationDbContext>();
+            //servisleirmiz
+            builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            //formlarımız
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<CompanyService>().As<ICompanyService>();
+            builder.RegisterType<ActivityService>().As<IActivityService>();
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var frm = scope.Resolve<FrmMain>();
+                Application.Run(frm);
+            }
         }
     }
 }
