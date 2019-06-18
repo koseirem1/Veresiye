3202be6fa04f24bd8ff4fa02c1659fcf0f7f3877
koseirem1 <koseirem1@gmail.com>
2019-06-18 13:52:27 +0300

    first version

---
+++ Veresiye.Service/ActivityService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veresiye.Data;
+using Veresiye.Model;
+using static Veresiye.Service.ActivityService;
+
+namespace Veresiye.Service
+{
+   public class ActivityService:IActivityService
+
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IRepository<Activity> activityRepository;
+
+        public ActivityService(IUnitOfWork unitOfWork, IRepository<Activity> ActivityRepository)
+        {
+            this.unitOfWork = unitOfWork;
+            this.activityRepository=activityRepository;
+        }
+
+        public void Delete(int id)
+        {
+            var activity = activityRepository.Get(id);
+            if (activity != null)
+            {
+                activityRepository.Delete(activity);
+                unitOfWork.SaveChanges();
+            }
+        }
+
+        public Activity Get(int id)
+        {
+            return activityRepository.Get(id);
+        }
+
+        public IEnumerable<Activity> GetAll()
+        {
+            return activityRepository.GetAll();
+        }
+
+        public void Insert(Activity activity)
+        {
+            activityRepository.Insert(activity);
+            unitOfWork.SaveChanges();
+        }
+
+        public void Update(Activity activity)
+        {
+            activityRepository.Update(activity);
+            unitOfWork.SaveChanges();
+        }
+
+        public interface IActivityService
+        {
+            void Insert(Activity activity); //crud işlemleri
+            void Update(Activity activity); //crud işlemleri
+            void Delete(int id); //crud işlemleri
+            IEnumerable<Activity> GetAll();
+            Activity Get(int id);
+        }
+    }
+}
